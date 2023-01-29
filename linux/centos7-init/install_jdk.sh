7a992c321271f6b1a1d9eaa72ec92518f88555de
BG-KGod <bzqwangyi2017@163.com>
2023-01-29 12:11:04 +0800

    jdk -install

---
+++ linux/centos7-init/install_jdk.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#安装jdk
+function install_java {
+ if ! command -v java &> /dev/null; then
+     echo "jdk未安装，是否准备安装 jdk1.8...（输入n不安装）"
+		read key
+		if [ ! "n" = "$key" ];then 	
+		  
+			#1.下载jdk
+			wget http://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
+			#2.解压
+			#z ：表示 tar 包是被 gzip 压缩过的 (后缀是.tar.gz)，所以解压时需要用 gunzip 解压 (.tar不需要)
+			#x ：表示 从 tar 包中把文件提取出来
+			#v ：表示 显示打包过程详细信息
+			#f ：指定被处理的文件是什么
+			tar -zxvf jdk-8u202-linux-x64.tar.gz
+			#3.创建jdk安装文件夹 并移动重命名到这个新文件夹
+			jdkDir=/KF-environment/jdk #java jdk 自定义安装路径
+			mkdir $jdkDir
+			mv jdk1.8.0_202 $jdkDir/jdk1.8
+			#4.配置环境变量
+			echo "
+#**********自定义配置开始*********
+# set java environment
+JAVA_HOME=${jdkDir}/jdk1.8
+PATH=\$JAVA_HOME/bin:\$PATH
+export JAVA_HOME PATH
+#**********自定义配置结束**********
+" >> /etc/profile
+			#5.刷新配置
+			source /etc/profile
+			echo "安装jdk成功并已经配置好环境变量 安装路径：$jdkDir/jdk1.8" 
+		else
+			echo "jdk安装失败" 
+		fi
+else 
+	
+	echo "jdk 已安装"
+
+ fi
+}
+#######执行程序#######
+install_java
--- linux/centos7-init/readme.txt
+++ linux/centos7-init/readme.txt
@@ -1,3 +1,9 @@
 1.只需要下载 install.start 这一个文件就可以实现起始环境的部署（无法读取远程的配置情况下只安装 ifconfig，vim，docker，wget）
 2.使用方式：将 install.start 拷贝到 一个自定义目录，使用chmod 设置执行权限 （如chmod u+x install.start）
 然后执行install.start文件
+3.install_jdk.sh 是Linux系统（基于centeros 7）安装 jdk (这里使用8u202版本 --作为Oracle JDK的这个版本是最终的免费商用版)
+jdk官网： https://www.oracle.com/java/technologies/downloads/#java8
+jdk华为镜像地址： https://repo.huaweicloud.com/java/jdk/
+jdk下载地址：https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
+jdk Linux下载命令： wget http://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
+4.
